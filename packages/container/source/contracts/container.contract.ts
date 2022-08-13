8ab6fda5bad81f47971cd8d6c6e97b4d20be8bd0
Endy Jasmi <endyjasmi@gmail.com>
2022-08-13 15:10:57 +0800

    feat(@marubase/container): add container contract

---
+++ packages/container/source/contracts/container.contract.ts
@@ -0,0 +1,65 @@
+import {
+  BindingKey,
+  BindingTag,
+  Callable,
+  Constructor,
+} from "./common.contract.js";
+import { RegistryInterface } from "./registry.contract.js";
+import { ResolverInterface } from "./resolver.contract.js";
+import { ScopeInterface } from "./scope.contract.js";
+
+export interface ContainerInterface {
+  readonly registry: RegistryInterface;
+
+  readonly scope: ScopeInterface;
+
+  bind(bindingKey: BindingKey): ContainerBinding;
+
+  bound(bindingKey: BindingKey): boolean;
+
+  call<ResolveInstance>(
+    callable: Callable<ResolveInstance>,
+    ...args: unknown[]
+  ): ResolveInstance;
+
+  create<ResolveInstance>(
+    constructor: Constructor<ResolveInstance>,
+    ...args: unknown[]
+  ): ResolveInstance;
+
+  fork(): this;
+
+  resolve<ResolveInstance>(
+    bindingKey: BindingKey,
+    ...args: unknown[]
+  ): ResolveInstance;
+
+  resolveTag<ResolveInstance>(
+    bindingTag: BindingTag,
+    ...args: unknown[]
+  ): ResolveInstance[];
+
+  unbind(bindingKey: BindingKey): this;
+}
+
+export type ContainerBinding = {
+  to<ResolveInstance>(
+    constructor: Constructor<ResolveInstance>,
+  ): ResolverInterface<ResolveInstance>;
+
+  toAlias<ResolveInstance>(
+    bindingKey: BindingKey,
+  ): ResolverInterface<ResolveInstance>;
+
+  toCallable<ResolveInstance>(
+    callable: Callable<ResolveInstance>,
+  ): ResolverInterface<ResolveInstance>;
+
+  toConstant<ResolveInstance>(
+    constant: ResolveInstance,
+  ): ResolverInterface<ResolveInstance>;
+
+  toTag<ResolveInstance>(
+    bindingTag: BindingTag,
+  ): ResolverInterface<ResolveInstance>;
+};
