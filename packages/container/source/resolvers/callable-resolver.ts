ed04afa0508fc8043c5c8c7cf14ad80e7f691d65
Endy Jasmi <endyjasmi@gmail.com>
2022-08-15 23:33:18 +0800

    feat(@marubase/container): add callable resolver implementation

---
+++ packages/container/source/resolvers/callable-resolver.ts
@@ -0,0 +1,43 @@
+import { BindingKey, Callable } from "../contracts/common.contract.js";
+import { RegistryInterface } from "../contracts/registry.contract.js";
+import { ResolverInterface } from "../contracts/resolver.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
+import { BaseResolver } from "./base-resolver.js";
+
+export class CallableResolver extends BaseResolver {
+  protected _callable: Callable;
+
+  public constructor(registry: RegistryInterface, callable: Callable) {
+    super(registry);
+    this._callable = callable;
+  }
+
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
+    const toInstance = (resolver: ResolverInterface): unknown =>
+      resolver.resolve(scope);
+    const toResolver = (bindingKey: BindingKey): ResolverInterface =>
+      this._registry.createBindingKeyResolver(bindingKey);
+    const callableArgs = this._dependencies
+      .map(toResolver)
+      .map(toInstance)
+      .concat(...args);
+    if (typeof this._callable === "function")
+      return this._callable(...callableArgs) as Result;
+
+    const [bindingKeyOrInstance, methodName] = this._callable;
+    try {
+      const bindingKey = bindingKeyOrInstance as BindingKey;
+      const instance = this._registry
+        .createBindingKeyResolver(bindingKey)
+        .resolve<Instance<Result>>(scope);
+      return instance[methodName](...callableArgs);
+    } catch (error) {
+      const instance = bindingKeyOrInstance as Instance<Result>;
+      return instance[methodName](...callableArgs);
+    }
+  }
+}
+
+export type Instance<Result> = {
+  [methodName: string | symbol]: (...args: unknown[]) => Result;
+};
--- packages/container/source/resolvers/index.ts
+++ packages/container/source/resolvers/index.ts
@@ -1,5 +1,6 @@
 export * from "./base-resolver.js";
 export * from "./binding-key-resolver.js";
 export * from "./binding-tag-resolver.js";
+export * from "./callable-resolver.js";
 export * from "./class-resolver.js";
 export * from "./constant-resolver.js";
