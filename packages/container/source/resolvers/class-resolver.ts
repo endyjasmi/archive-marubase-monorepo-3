1b5ddb91b48489fef04fd7d19d4920075106299b
Endy Jasmi <endyjasmi@gmail.com>
2022-08-15 23:09:31 +0800

    refactor(@marubase/container): update class resolver implementation

--- packages/container/source/resolvers/class-resolver.ts
+++ packages/container/source/resolvers/class-resolver.ts
@@ -12,15 +12,12 @@ export class ClassResolver extends BaseResolver {
     this._constructor = constructor;
   }
 
-  public resolve<Instance>(
-    scope: ScopeInterface,
-    ...args: unknown[]
-  ): Instance {
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
     const toInstance = (resolver: ResolverInterface): unknown =>
       resolver.resolve(scope);
     const toResolver = (bindingKey: BindingKey): ResolverInterface =>
       this._registry.createBindingKeyResolver(bindingKey);
-    const constructor = this._constructor as Constructor<Instance>;
+    const constructor = this._constructor as Constructor<Result>;
     const constructorArgs = this._dependencies
       .map(toResolver)
       .map(toInstance)
