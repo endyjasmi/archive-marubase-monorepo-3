08ec33924dd497e26ecfda2f438631f3c5b3d140
Endy Jasmi <endyjasmi@gmail.com>
2022-08-15 12:19:12 +0800

    refactor(@marubase/container): update common contract

--- packages/container/source/contracts/common.contract.ts
+++ packages/container/source/contracts/common.contract.ts
@@ -2,17 +2,11 @@ export type BindingKey = Function | string | symbol;
 
 export type BindingTag = string | symbol;
 
-export type Callable<Result> =
-  | [BindingKey, MethodName]
-  | [Object, MethodName]
-  | ((...args: unknown[]) => Result);
+export type Callable =
+  | [BindingKey, string | symbol]
+  | [Object, string | symbol]
+  | (<Result>(...args: unknown[]) => Result);
 
 export type Constructor<Instance> = {
   new (...args: unknown[]): Instance;
 };
-
-export type Instance<Result> = {
-  [methodName: MethodName]: (...args: unknown[]) => Result;
-};
-
-export type MethodName = string | symbol;
