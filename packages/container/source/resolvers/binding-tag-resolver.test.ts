3b202efc17dc8db080672cea05f6d958fc2a7c0a
Endy Jasmi <endyjasmi@gmail.com>
2022-08-15 23:03:36 +0800

    test(@marubase/container): update binding tag resolver test

--- packages/container/source/resolvers/binding-tag-resolver.test.ts
+++ packages/container/source/resolvers/binding-tag-resolver.test.ts
@@ -1,41 +1,43 @@
 import { expect } from "chai";
+import { BindingTag } from "../contracts/common.contract.js";
 import { Registry } from "../registry.js";
 import { Scope } from "../scope.js";
 import { BindingTagResolver } from "./binding-tag-resolver.js";
 
-describe("BindingTagResolver", function () {
-  describe("#resolve(scope)", function () {
-    context("when binding tag is string", function () {
-      let bindingTag: string;
-      let registry: Registry;
-      let resolver: BindingTagResolver;
-      beforeEach(function () {
-        bindingTag = "tag";
-        registry = new Registry();
-        resolver = new BindingTagResolver(registry, bindingTag);
-      });
+const bindingTags: BindingTag[] = [Symbol("Date"), "Date"];
+bindingTags.forEach((bindingTag) => {
+  const bindingTagName =
+    typeof bindingTag !== "string"
+      ? `Symbol(${bindingTag.toString()})`
+      : bindingTag;
+  describe(`BindingTagResolver (${bindingTagName})`, function () {
+    let registry: Registry;
+    let resolver: BindingTagResolver;
+    beforeEach(() => (registry = new Registry()));
+    beforeEach(() => (resolver = new BindingTagResolver(registry, bindingTag)));
 
-      it("should return instance array", function () {
-        const scope = new Scope();
-        const instances = resolver.resolve(scope);
-        expect(instances).to.be.an("array");
+    describe("#resolve(scope, ...args)", function () {
+      context("when there is binding", function () {
+        beforeEach(() => {
+          registry
+            .createClassResolver(Date)
+            .setBindingKey(Date)
+            .setBindingTags(bindingTag);
+        });
+        it("should return instance array", function () {
+          const scope = new Scope();
+          const instances = resolver.resolve<Date[]>(scope);
+          expect(instances).to.be.an("array");
+          expect(instances[0]).to.be.an.instanceOf(Date);
+        });
       });
-    });
-
-    context("when binding tag is symbol", function () {
-      let bindingTag: symbol;
-      let registry: Registry;
-      let resolver: BindingTagResolver;
-      beforeEach(function () {
-        bindingTag = Symbol("tag");
-        registry = new Registry();
-        resolver = new BindingTagResolver(registry, bindingTag);
-      });
-
-      it("should return instance array", function () {
-        const scope = new Scope();
-        const instances = resolver.resolve(scope);
-        expect(instances).to.be.an("array");
+      context("when there is no binding", function () {
+        it("should return empty array", function () {
+          const scope = new Scope();
+          const instances = resolver.resolve<Date[]>(scope);
+          expect(instances).to.be.an("array");
+          expect(instances).to.be.empty;
+        });
       });
     });
   });
