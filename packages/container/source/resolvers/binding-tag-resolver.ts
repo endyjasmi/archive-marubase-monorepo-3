fc7efc3a397e361d5f8c6d09b35ad46838efb1df
Endy Jasmi <endyjasmi@gmail.com>
2022-08-15 13:14:07 +0800

    feat(@marubase/container): add binding tag resolver implementation

---
+++ packages/container/source/resolvers/binding-tag-resolver.ts
@@ -0,0 +1,22 @@
+import { BindingTag } from "../contracts/common.contract.js";
+import { RegistryInterface } from "../contracts/registry.contract.js";
+import { ResolverInterface } from "../contracts/resolver.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
+import { BaseResolver } from "./base-resolver.js";
+
+export class BindingTagResolver extends BaseResolver {
+  protected _resolveTag: BindingTag;
+
+  public constructor(registry: RegistryInterface, bindingTag: BindingTag) {
+    super(registry);
+    this._resolveTag = bindingTag;
+  }
+
+  public resolve<Instance>(scope: ScopeInterface): Instance {
+    const toInstance = (resolver: ResolverInterface): unknown =>
+      resolver.resolve(scope);
+    return this._registry
+      .getResolverByTag(this._resolveTag)
+      .map(toInstance) as unknown as Instance;
+  }
+}
--- packages/container/source/resolvers/index.ts
+++ packages/container/source/resolvers/index.ts
@@ -1,2 +1,3 @@
 export * from "./base-resolver.js";
 export * from "./binding-key-resolver.js";
+export * from "./binding-tag-resolver.js";
