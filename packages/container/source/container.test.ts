20b0e242c0557c5acc15301a5455b00d2268f158
Endy Jasmi <endyjasmi@gmail.com>
2022-08-16 14:46:20 +0800

    test(@marubase/container): update container test

--- packages/container/source/container.test.ts
+++ packages/container/source/container.test.ts
@@ -1,5 +1,8 @@
-import { expect } from "chai";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 import { Container } from "./container.js";
+import { ProviderInterface } from "./contracts/provider.contract.js";
+import { ContainerError } from "./errors/container.error.js";
 import { ResolverError } from "./errors/resolver.error.js";
 import { Registry } from "./registry.js";
 import { BindingKeyResolver } from "./resolvers/binding-key-resolver.js";
@@ -9,9 +12,79 @@ import { ClassResolver } from "./resolvers/class-resolver.js";
 import { ConstantResolver } from "./resolvers/constant-resolver.js";
 import { Scope } from "./scope.js";
 
+const { expect } = chai;
+chai.use(chaiAsPromised);
+
 describe("Container", function () {
   let container: Container;
   beforeEach(() => (container = new Container()));
+
+  describe("get booted()", function () {
+    context("when invoke from root container", function () {
+      context("and container is booted", function () {
+        beforeEach(async () => container.boot());
+        it("should return true", function () {
+          const booted = container.booted;
+          expect(booted).to.be.true;
+        });
+      });
+      context("and container is not booted", function () {
+        it("should return false", function () {
+          const booted = container.booted;
+          expect(booted).to.be.false;
+        });
+      });
+    });
+    context("when invoke from branch container", function () {
+      let branch: Container;
+      beforeEach(() => (branch = container.fork()));
+      context("and container is booted", function () {
+        beforeEach(async () => container.boot());
+        it("should return true", function () {
+          const booted = branch.booted;
+          expect(booted).to.be.true;
+        });
+      });
+      context("and container is not booted", function () {
+        it("should return false", function () {
+          const booted = branch.booted;
+          expect(booted).to.be.false;
+        });
+      });
+    });
+  });
+
+  describe("get parent()", function () {
+    context("when invoke from root container", function () {
+      it("should return undefined", function () {
+        const parent = container.parent;
+        expect(parent).to.be.undefined;
+      });
+    });
+    context("when invoke from branch container", function () {
+      it("should return parent", function () {
+        const branch = container.fork();
+        const parent = branch.parent;
+        expect(parent).to.equal(container);
+      });
+    });
+  });
+
+  describe("get providers()", function () {
+    context("when invoke from root container", function () {
+      it("should return providers", function () {
+        const providers = container.providers;
+        expect(providers).to.deep.equal({});
+      });
+    });
+    context("when invoke from branch container", function () {
+      it("should return providers", function () {
+        const branch = container.fork();
+        const providers = branch.providers;
+        expect(providers).to.deep.equal({});
+      });
+    });
+  });
 
   describe("get registry()", function () {
     it("should return registry", function () {
@@ -62,6 +135,45 @@ describe("Container", function () {
     });
   });
 
+  describe("#boot()", function () {
+    context("when container is not booted", function () {
+      context("and there is provider", function () {
+        let provider: ProviderInterface;
+        beforeEach(() => (provider = {}));
+        beforeEach(() => container.install("test", provider));
+        context("with boot method", function () {
+          beforeEach(() => (provider.boot = () => Promise.resolve()));
+          it("should run boot", async function () {
+            await container.boot();
+          });
+        });
+        context("with no boot method", function () {
+          it("should run boot", async function () {
+            await container.boot();
+          });
+        });
+      });
+      context("and there is no provider", function () {
+        it("should run boot", async function () {
+          await container.boot();
+        });
+      });
+    });
+    context("when container is booted", function () {
+      beforeEach(async () => container.boot());
+      it("should run boot", async function () {
+        await container.boot();
+      });
+    });
+    context("when invoke from branch", function () {
+      it("should throw container error", async function () {
+        const branch = container.fork();
+        const promise = branch.boot();
+        return expect(promise).to.eventually.rejectedWith(ContainerError);
+      });
+    });
+  });
+
   describe("#bound(bindingKey)", function () {
     context("when there is binding", function () {
       beforeEach(() => container.bind("key").toClass(Date));
@@ -97,6 +209,106 @@ describe("Container", function () {
       const fork = container.fork();
       expect(fork).to.be.an.instanceOf(Container);
       expect(fork).to.not.equal(container);
+    });
+  });
+
+  describe("#install(name, provider)", function () {
+    let provider: ProviderInterface;
+    beforeEach(() => (provider = {}));
+    context("when container is booted", function () {
+      beforeEach(async () => container.boot());
+      context("and there is provider", function () {
+        beforeEach(() => container.install("test", provider));
+        it("should throw container error", function () {
+          const runFn = () => container.install("test", provider);
+          expect(runFn).to.throw(ContainerError);
+        });
+      });
+      context("and there is no provider", function () {
+        context("and supplied provider with boot method", function () {
+          beforeEach(() => (provider.boot = () => Promise.resolve()));
+          it("should return self", function () {
+            const self = container.install("test", provider);
+            expect(self).to.equal(container);
+          });
+        });
+        context("and supplied provider with no boot method", function () {
+          it("should return self", function () {
+            const self = container.install("test", provider);
+            expect(self).to.equal(container);
+          });
+        });
+      });
+    });
+    context("when container is not booted", function () {
+      context("and there is provider", function () {
+        beforeEach(() => container.install("test", provider));
+        it("should throw container error", function () {
+          const runFn = () => container.install("test", provider);
+          expect(runFn).to.throw(ContainerError);
+        });
+      });
+      context("and there is no provider", function () {
+        context("and supplied provider with install method", function () {
+          beforeEach(() => (provider.install = () => undefined));
+          it("should return self", function () {
+            const self = container.install("test", provider);
+            expect(self).to.equal(container);
+          });
+        });
+        context("and supplied provider with no install method", function () {
+          it("should return self", function () {
+            const self = container.install("test", provider);
+            expect(self).to.equal(container);
+          });
+        });
+      });
+    });
+    context("when invoke from branch container", function () {
+      it("should throw container error", function () {
+        const branch = container.fork();
+        const runFn = () => branch.install("test", provider);
+        expect(runFn).to.throw(ContainerError);
+      });
+    });
+  });
+
+  describe("#installed(name)", function () {
+    context("when invoke from root container", function () {
+      context("when there is provider", function () {
+        let provider: ProviderInterface;
+        beforeEach(() => (provider = {}));
+        beforeEach(() => container.install("test", provider));
+        it("should return true", function () {
+          const installed = container.installed("test");
+          expect(installed).to.be.true;
+        });
+      });
+      context("when there is no provider", function () {
+        it("should return false", function () {
+          const installed = container.installed("test");
+          expect(installed).to.be.false;
+        });
+      });
+    });
+    context("when invoke from branch container", function () {
+      let branch: Container;
+      beforeEach(() => (branch = container.fork()));
+      context("when there is provider", function () {
+        let provider: ProviderInterface;
+        beforeEach(() => (provider = {}));
+        beforeEach(() => container.install("test", provider));
+        it("should return true", function () {
+          const installed = branch.installed("test");
+          expect(installed).to.be.true;
+        });
+      });
+      context("when there is no provider", function () {
+        it("should return false", function () {
+          const installed = branch.installed("test");
+          expect(installed).to.be.false;
+        });
+      });
     });
   });
 
@@ -138,6 +350,45 @@ describe("Container", function () {
     });
   });
 
+  describe("#shutdown()", function () {
+    context("when container is booted", function () {
+      beforeEach(async () => container.boot());
+      context("and there is provider", function () {
+        let provider: ProviderInterface;
+        beforeEach(() => (provider = {}));
+        beforeEach(() => container.install("test", provider));
+        context("with shutdown method", function () {
+          beforeEach(() => (provider.shutdown = () => Promise.resolve()));
+          it("should run shutdown", async function () {
+            await container.shutdown();
+          });
+        });
+        context("with no shutdown method", function () {
+          it("should run shutdown", async function () {
+            await container.shutdown();
+          });
+        });
+      });
+      context("and there is no provider", function () {
+        it("should run shutdown", async function () {
+          await container.shutdown();
+        });
+      });
+    });
+    context("when container is not booted", function () {
+      it("should run shutdown", async function () {
+        await container.shutdown();
+      });
+    });
+    context("when invoke from branch", function () {
+      it("should run shutdown", async function () {
+        const branch = container.fork();
+        const promise = branch.shutdown();
+        return expect(promise).to.eventually.rejectedWith(ContainerError);
+      });
+    });
+  });
+
   describe("#unbind(bindingKey)", function () {
     context("when there is binding", function () {
       beforeEach(() => container.bind("key").toClass(Date));
@@ -150,6 +401,77 @@ describe("Container", function () {
       it("should return self", function () {
         const self = container.unbind("key");
         expect(self).to.equal(container);
+      });
+    });
+  });
+
+  describe("#uninstall(name)", function () {
+    context("when container is booted", function () {
+      beforeEach(async () => container.boot());
+      context("and there is provider", function () {
+        let provider: ProviderInterface;
+        beforeEach(() => (provider = {}));
+        beforeEach(() => container.install("test", provider));
+        context("with shutdown method", function () {
+          beforeEach(() => (provider.shutdown = () => Promise.resolve()));
+          it("should return self", function () {
+            const self = container.uninstall("test");
+            expect(self).to.equal(container);
+          });
+        });
+        context("with no shutdown method", function () {
+          it("should return self", function () {
+            const self = container.uninstall("test");
+            expect(self).to.equal(container);
+          });
+        });
+        context("with shutdown and uninstall method", function () {
+          beforeEach(() => (provider.shutdown = () => Promise.resolve()));
+          beforeEach(() => (provider.uninstall = () => undefined));
+          it("should return self", function () {
+            const self = container.uninstall("test");
+            expect(self).to.equal(container);
+          });
+        });
+      });
+      context("and there is no provider", function () {
+        it("should return self", function () {
+          const self = container.uninstall("test");
+          expect(self).to.equal(container);
+        });
+      });
+    });
+    context("when container is not booted", function () {
+      context("and there is provider", function () {
+        let provider: ProviderInterface;
+        beforeEach(() => (provider = {}));
+        beforeEach(() => container.install("test", provider));
+        context("with uninstall method", function () {
+          beforeEach(() => (provider.uninstall = () => undefined));
+          it("should return self", function () {
+            const self = container.uninstall("test");
+            expect(self).to.equal(container);
+          });
+        });
+        context("with no uninstall method", function () {
+          it("should return self", function () {
+            const self = container.uninstall("test");
+            expect(self).to.equal(container);
+          });
+        });
+      });
+      context("and there is no provider", function () {
+        it("should return self", function () {
+          const self = container.uninstall("test");
+          expect(self).to.equal(container);
+        });
+      });
+    });
+    context("when invoke from branch", function () {
+      it("should throw container error", function () {
+        const branch = container.fork();
+        const runFn = () => branch.uninstall("test");
+        expect(runFn).to.throw(ContainerError);
       });
     });
   });
