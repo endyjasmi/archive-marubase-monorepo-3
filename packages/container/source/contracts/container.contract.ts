551b528c7b1680dcb88fc1da65100005de3f9ce8
Endy Jasmi <endyjasmi@gmail.com>
2022-08-16 13:19:31 +0800

    refactor(@marubase/container): update container contract

--- packages/container/source/contracts/container.contract.ts
+++ packages/container/source/contracts/container.contract.ts
@@ -1,14 +1,23 @@
 import { BindingKey, BindingTag, Callable } from "./common.contract.js";
+import { ProviderInterface, ProviderName } from "./provider.contract.js";
 import { RegistryInterface } from "./registry.contract.js";
 import { ResolverInterface } from "./resolver.contract.js";
 import { ScopeInterface } from "./scope.contract.js";
 
 export interface ContainerInterface {
+  readonly booted: boolean;
+
+  readonly parent?: this;
+
+  readonly providers: Record<ProviderName, ProviderInterface>;
+
   readonly registry: RegistryInterface;
 
   readonly scope: ScopeInterface;
 
   bind(bindingKey: BindingKey): ContainerBinding;
+
+  boot(): Promise<void>;
 
   bound(bindingKey: BindingKey): boolean;
 
@@ -18,11 +27,19 @@ export interface ContainerInterface {
 
   fork(): this;
 
+  install(name: ProviderName, provider: ProviderInterface): this;
+
+  installed(name: ProviderName): boolean;
+
   resolve<Result>(bindingKey: BindingKey, ...args: unknown[]): Result;
 
   resolveTag<Result>(bindingTag: BindingTag): Result[];
 
+  shutdown(): Promise<void>;
+
   unbind(bindingKey: BindingKey): this;
+
+  uninstall(name: ProviderName): this;
 }
 
 export type ContainerBinding = {
