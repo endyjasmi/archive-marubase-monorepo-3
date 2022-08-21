ee5a55a40d123b3d6bbcd2968530d4dee57f5143
Endy Jasmi <endyjasmi@gmail.com>
2022-08-21 21:42:46 +0800

    feat(@marubase/framework): add message contract

---
+++ packages/framework/source/contracts/message.contract.ts
@@ -0,0 +1,30 @@
+import { Readable } from "node:stream";
+
+export interface MessageInterface {
+  readonly body: Readable;
+
+  readonly headers: Map<string, string>;
+
+  readonly protocol: MessageProtocol;
+
+  clearBody(): this;
+
+  clearHeader(key: string): this;
+
+  clearHeaders(): this;
+
+  setBody(stream: Readable): this;
+
+  setHeader(key: string, value: string): this;
+
+  setHeaders(entries: [string, string][]): this;
+
+  setProtocol(protocol: MessageProtocol): this;
+}
+
+export type MessageProtocol =
+  | "HTTP/0.9"
+  | "HTTP/1.0"
+  | "HTTP/1.1"
+  | "HTTP/2"
+  | "HTTP/3";
